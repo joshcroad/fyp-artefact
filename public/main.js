fefa785fbf5e1e9b67c886c402f9fd5f7d6e8435
Joshua Croad <joshcroad@gmail.com>
2014-03-10 13:16:54 +0000

    Playing with d3 graphs with new results

--- public/main.js
+++ public/main.js
@@ -1,8 +1,10 @@
-var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
+var margin = {top: 20, right: 80, bottom: 30, left: 100},
+    width = 1400 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var x = d3.scale.linear()
+var parseDate = d3.time.format("%Y%m%d").parse;
+
+var x = d3.time.scale()
     .range([0, width]);
 
 var y = d3.scale.linear()
@@ -18,70 +20,57 @@ var yAxis = d3.svg.axis()
     .scale(y)
     .orient("left");
 
+var line = d3.svg.line()
+    .x(function(d) { return x(d.records); })
+    .y(function(d) { return y(d.avgtime); });
+
 var svg = d3.select("body").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.tsv("data.tsv", function(error, data) {
-  data.forEach(function(d) {
-    console.log(d);
-    d.time = +d.time;
-    d.round = +d.round;
+d3.csv("data-search.csv", function(error, data) {
+  color.domain(d3.keys(data[0]).filter(function(key) { return key !== "records"; }));
+
+  var databases = color.domain().map(function(name) {
+    return {
+      name: name,
+      values: data.map(function(d) {
+        return {records: d.records, avgtime: +d[name]};
+      })
+    };
   });
 
-  x.domain(d3.extent(data, function(d) { return d.time; })).nice();
-  y.domain(d3.extent(data, function(d) { return d.round; })).nice();
+  x.domain([10, 100000]);
+
+  y.domain([
+    d3.min(databases, function(c) { return d3.min(c.values, function(v) { return v.avgtime; }); }),
+    d3.max(databases, function(c) { return d3.max(c.values, function(v) { return v.avgtime; }); })
+  ]);
 
   svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-    .append("text")
-      .attr("class", "label")
-      .attr("x", width)
-      .attr("y", -6)
-      .style("text-anchor", "end")
-      .text("Iteration");
+      .call(xAxis);
 
   svg.append("g")
       .attr("class", "y axis")
       .call(yAxis)
     .append("text")
-      .attr("class", "label")
       .attr("transform", "rotate(-90)")
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Time (nanoseconds)")
+      .text("Average Time (nanoseconds)");
 
-  svg.selectAll(".dot")
-      .data(data)
-    .enter().append("circle")
-      .attr("class", "dot")
-      .attr("r", 3.5)
-      .attr("cx", function(d) { return x(d); })
-      .attr("cy", function(d) { return y(d.round); })
-      .style("fill", function(d) { return color(d.database); });
-
-  var legend = svg.selectAll(".legend")
-      .data(color.domain())
+  var database = svg.selectAll(".database")
+      .data(databases)
     .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+      .attr("class", "database");
 
-  legend.append("rect")
-      .attr("x", width - 18)
-      .attr("width", 18)
-      .attr("height", 18)
-      .style("fill", color);
-
-  legend.append("text")
-      .attr("x", width - 24)
-      .attr("y", 9)
-      .attr("dy", ".35em")
-      .style("text-anchor", "end")
-      .text(function(d) { return d; });
-
+  database.append("path")
+      .attr("class", "line")
+      .attr("d", function(d) { return line(d.values); })
+      .style("stroke", function(d) { return color(d.name); });
 });
